92855fc342f7b87b8c01c3f00f608f257c40c926
Ludis-all <olyn.fershee@proton.me>
2026-04-11 13:35:14 -0700

    timer starts on read

--- countdown.gd
+++ countdown.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+var gameOver = false
+
 func _on_timer_timeout() -> void:
 	get_child(1).visible = false
 	get_child(1).set_process(PROCESS_MODE_DISABLED)
 	get_child(0).get_child(0).visible = true
 	get_child(0).get_child(1).visible = false
 	get_child(0).get_child(1).set_process(PROCESS_MODE_DISABLED)
+	gameOver = true
+
+func _process(delta: float) -> void:
+	if (gameOver && Input.is_action_just_pressed("Restart")):
+		get_tree().reload_current_scene()
--- displayText.gd
+++ displayText.gd
@@ -1,16 +1,32 @@
 extends RichTextLabel
 
-@onready var currentManual = "res://text.txt";
-
+@onready var currentManual = "res://topSecret.txt"
+@onready var page = 1
+@onready var timer = get_parent().get_parent().get_child(3)
 
 func _on_clicker_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event.is_action_released("Left Click")):
-		currentManual = "res://text.txt"
+		currentManual = "res://topSecret.txt"
 		displayPage()
 
 func displayPage() -> void:
 	clear()
 	var file = FileAccess.open(currentManual, FileAccess.READ)
-	while (get_line_count() < 16 && !file.eof_reached()):
-		append_text(file.get_line())
+	var contents = file.get_line()
+	while (!contents.contains("Page " + str(page)) && !file.eof_reached()):
+		contents = file.get_line()
+	contents = file.get_line()
+	while (!contents.contains("Page " + str(page + 1)) && !file.eof_reached()):
+		append_text(contents)
 		newline()
+		contents = file.get_line()
+
+func _process(delta: float) -> void:
+	if (Input.is_action_just_released("Left") && page > 1):
+		page -= 1
+	if (Input.is_action_just_released("Right")):
+		page += 1
+	displayPage()
+	
+	if (currentManual == "res://topSecret.txt" && page == 3 && timer.is_stopped()):
+		timer.start()
